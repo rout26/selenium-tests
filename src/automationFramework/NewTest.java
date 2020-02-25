09eba8dd4c940ad394675f72aa9eb67dd9a8e392
routsu <routsu@ZZroutsu01.microfocus.com>
2020-02-25 16:07:34 +0530

    commit

--- src/automationFramework/NewTest.java
+++ src/automationFramework/NewTest.java
@@ -14,7 +14,7 @@ public class NewTest {
   @Test
   public void verifyHomepageTitle() {
       
-      System.out.println("launching firefox browser"); 
+      System.out.println("launching firefox browser");
       System.setProperty("webdriver.gecko.driver", driverPath);
       driver = new FirefoxDriver();
       driver.get(baseUrl);
