188d768b9a9c6301b9c678f049bbf5c5157eb879
Rout <theRout@gmail.com>
2020-02-25 12:39:11 +0530

    Test upload

---
+++ src/automationFramework/FirstTest.java
@@ -0,0 +1,27 @@
+package automationFramework;
+/*import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class FirstTest {
+ 
+ public static void main(String[] args) {
+ 
+ // Create a new instance of the Firefox driver
+System.setProperty("webdriver.gecko.driver","C:\\Users\\routsu\\Documents\\DevTools\\selenium\\Drivers\\geckodriver.exe");
+WebDriver driver = new FirefoxDriver();
+ 
+        //Launch the Online Store Website
+ driver.get("http://www.shop.demoqa.com");
+ 
+        // Print a Log In message to the screen
+        System.out.println("Successfully opened the website www.Store.Demoqa.com");
+ 
+ //Wait for 5 Sec
+ //Thread.sleep(5000);
+ 
+        // Close the driver
+        driver.quit();
+    }
+}
+
+*/
---
+++ src/automationFramework/NewTest.java
@@ -0,0 +1,26 @@
+package automationFramework;
+
+import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.AssertJUnit;
+import org.testng.annotations.Test;
+
+public class NewTest {
+	
+	public String baseUrl = "http://demo.guru99.com/test/newtours/";
+    String driverPath = "C:\\Users\\routsu\\Documents\\DevTools\\selenium\\Drivers\\geckodriver.exe";
+    public WebDriver driver ; 
+  @Test
+  public void verifyHomepageTitle() {
+      
+      System.out.println("launching firefox browser"); 
+      System.setProperty("webdriver.gecko.driver", driverPath);
+      driver = new FirefoxDriver();
+      driver.get(baseUrl);
+      String expectedTitle = "Welcome: Mercury Tours";
+      String actualTitle = driver.getTitle();
+      AssertJUnit.assertEquals(actualTitle, expectedTitle);
+      driver.close();
+  }
+}
